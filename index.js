4229a1135a3e257a37da221d020a48bdf14d2932
DaniCutler <72281065+DaniCutler@users.noreply.github.com>
2020-12-15 15:36:47 -0700

    placed markdown js into utilities and...
    added questions and dependencies to index.js

--- index.js
+++ index.js
@@ -1,6 +1,71 @@
+// modules
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utilities/generateMarkdown.js");
+
+
+
+
 // array of questions for user
 const questions = [
+    {
+        type:"input",
+        name:"name",
+        message:"What is your full name?",
+    },
+    {
+        type:"input",
+        name:"name",
+        message:"What is your full name?",
+    },
+    {
+        type:"input",
+        name:"name",
+        message:"What is your full name?",
+    },
+    {
+        type:"input",
+        name:"name",
+        message:"What is your full name?",
+    },
+    {
+        type:"input",
+        name:"name",
+        message:"What is your full name?",
+    },
+    {
+        type:"input",
+        name:"name",
+        message:"What is your full name?",
+    },
 
+    {
+        type:"input",
+        name:"name",
+        message:"What is your full name?",
+    },
+
+{
+        type:"input",
+        name:"name",
+        message:"What is your full name?",
+    },
+    {
+        type:"input",
+        name:"name",
+        message:"What is your full name?",
+    },
+    {
+        type:"input",
+        name:"name",
+        message:"What is your full name?",
+    },
+    {
+        type:"input",
+        name:"name",
+        message:"What is your full name?",
+    }
 ];
 
 // function to write README file
