32b275a12fc5658857111f8ab066b4431631e423
DaniCutler <72281065+DaniCutler@users.noreply.github.com>
2020-12-15 16:06:33 -0700

    updated markdown with license logo grab

--- utilities/generateMarkdown.js
+++ utilities/generateMarkdown.js
@@ -1,6 +1,33 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+
+// gets license info
+data.licenseLogo = getLicenseLogo(data.license)
+
+
+// when user selects a certain license, the logo is grabbed from a link
+try {
+  if (license === "APACHE 2.0") {
+    return "![Github license](https://img.shields.io/badge/License-Apache-2.svg)";
+  }
+  if (license === "MIT") {
+    return "![Github license](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  }
+  if (license === "BSD 3") {
+    return "![Github license](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)";
+  }
+  if (license === "GPL 3.0") {
+    return "![Github license](https://img.shields.io/badge/License-GPL-3.svg)";
+  }
+  if (license === "NONE") {
+    return ""
+  }
+
+} catch (error) {
+  console.log(error);
+ }
+}
+return `# ${data.title}
 
 `;
 }
